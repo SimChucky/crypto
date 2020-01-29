c6dae7525eda558a4a2943027df95842d8cbbf61
dhofmann <dhofmann@wp-54.sit.fraunhofer.de>
2020-01-29 19:24:23 +0100

    - add parameters and test output for frodoKEM parameter sets

--- org.jcryptool.visual.lwe/src/org/jcryptool/visual/lwe/algorithm/AlgorithmParameters.java
+++ org.jcryptool.visual.lwe/src/org/jcryptool/visual/lwe/algorithm/AlgorithmParameters.java
@@ -8,13 +8,11 @@ package org.jcryptool.visual.lwe.algorithm;
  */
 public class AlgorithmParameters {
 
-<<<<<<< HEAD
     /** n ≡ 0 (mod 8) the main parameter **/
     public int no;
     /** a power-of-two integer modulus with exponent D ≤ 16 !! minus one for bit masking **/
     public int q; 
     public int D;
-    public int stripeStep;
     public int nbar;
     /** the number of bits encoded in each matrix entry **/
     public int B;
@@ -32,20 +30,64 @@ public class AlgorithmParameters {
     /** (logQ * N * nbar)/8 + (loqQ * nbar * nbar)/8 **/
     public int cypherTextBytes;
     public int lenX;
+    
+    
+    public static AlgorithmParameters frodo640() {
+        AlgorithmParameters params = new AlgorithmParameters();
+        params.no = 640;
+        params.D = 15;
+        params.q = 0x7fff;
+        params.B = 2;
+        params.nbar = 8;
+        params.lenSeedA = 16;
+        params.cryptoBytes = 16;
+        params.lenX = 2;
+        params.bytesMU = (params.B * params.nbar * params.nbar) / 8;
+        params.CDF_TABLE = new int[] { 4643, 13363, 20579, 25843, 29227, 31145, 32103, 32525, 32689, 32745, 32762,
+                32766, 32767 };
+        params.secretKeyBytes = 19888; // sizeof(s) + CRYPTO_PUBLICKEYBYTES + 2*PARAMS_N*PARAMS_NBAR
+                                       // + BYTES_PKHASH
+        params.publicKeyBytes = 9616; // sizeof(seed_A) + (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8
+        params.cypherTextBytes = 9720;
+        return params;
+    }
+    
+    public static AlgorithmParameters frodo976() {
 
-=======
-    public int no;     // n ≡ 0 (mod 8) the main parameter
-    public int q;      // a power-of-two integer modulus with exponent D ≤ 16 !! minus one for bit masking
-    public int logQ;      // a power
-    public int stripeStep, nbar;   // integer matrix dimensions with
-    public int extractedBits;      // the number of bits encoded in each matrix entry
-    public int l;      // B·m·n, the length of bit strings that are encoded as m-by-n matrices
-    public int lenSeedA; // the byte length of seed used for pseudorandom pk-matrix generation
-    public short[] CDF_TABLE;    // a probability distribution on Z, rounded Gaussian distribution
-    public int bytesMU;
-    public int cryptoBytes;
-    public int secretKeyBytes;
-    public int publicKeyBytes;
-   
->>>>>>> refs/remotes/origin/lwe
+        AlgorithmParameters params = new AlgorithmParameters();
+        params.no = 976;
+        params.q = 0xffff;
+        params.D = 16;
+        params.B = 3;
+        params.nbar = 8;
+        params.lenSeedA = 16;
+        params.cryptoBytes = 24;
+        params.lenX = 2;
+        params.CDF_TABLE =new int[]{5638, 15915, 23689, 28571, 31116, 32217, 32613, 32731, 32760, 32766, 32767};
+        params.bytesMU = (params.B * params.nbar * params.nbar) / 8;
+        params.secretKeyBytes = 31296;
+        params.publicKeyBytes = 15632; 
+        params.cypherTextBytes = 15744;
+        return params;
+    }
+    // Frodo1344 returns Parameters struct no.1344
+    public static AlgorithmParameters frodo1344() {
+
+        AlgorithmParameters params = new AlgorithmParameters();
+
+        params.no = 1344;
+        params.q = 0xffff;
+        params.D = 16;
+        params.B = 4;
+        params.nbar = 8;
+        params.lenSeedA = 16;
+        params.cryptoBytes = 32;
+        params.lenX = 2;
+        params.CDF_TABLE = new int[]{9142, 23462, 30338, 32361, 32725, 32765, 32767};
+        params.bytesMU = (params.B * params.nbar * params.nbar) / 8;
+        params.secretKeyBytes = 43088;
+        params.publicKeyBytes = 21520; 
+        params.cypherTextBytes = 21632;
+        return params;
+    }
 }
--- org.jcryptool.visual.lwe/src/org/jcryptool/visual/lwe/algorithm/FrodoKEM.java
+++ org.jcryptool.visual.lwe/src/org/jcryptool/visual/lwe/algorithm/FrodoKEM.java
@@ -4,7 +4,8 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 
 import org.bouncycastle.crypto.digests.SHAKEDigest;
-<<<<<<< HEAD
+import org.bouncycastle.util.Strings;
+import org.bouncycastle.util.encoders.Hex;
 import org.eclipse.core.runtime.Assert;
 
 public class FrodoKEM {
@@ -22,44 +23,60 @@ public class FrodoKEM {
     private byte[] mu;
 
     public static void main(String[] args) {
-        FrodoKEM frodo = new FrodoKEM();
+        FrodoKEM frodo = new FrodoKEM(AlgorithmParameters.frodo640());
+
         frodo.createKeypair();
-                        
         byte[] ss1 = frodo.keyEncap();
         byte[] ss2 = frodo.keyDecap();
         if (!Arrays.equals(ss1, ss2)) {
             throw new RuntimeException("Shared secrets do not match");
         }
+        
+        System.out.println("FrodoKEM 640");
+        System.out.println("Auto-generated mu:\t" + Hex.toHexString(frodo.getMu()));
+        System.out.println("Alices shared secret:\t" + Hex.toHexString(ss1));
+        System.out.println("Bobs shared secret:\t" + Hex.toHexString(ss2)+"\n");
+        
+        frodo.setParams(AlgorithmParameters.frodo976());
+        frodo.createKeypair();
+        ss1 = frodo.keyEncap();
+        ss2 = frodo.keyDecap();
+        if (!Arrays.equals(ss1, ss2)) {
+            throw new RuntimeException("Shared secrets do not match");
+        }
+        
+        System.out.println("FrodoKEM 976");
+        System.out.println("Auto-generated mu:\t" + Hex.toHexString(frodo.getMu()));
+        System.out.println("Alices shared secret:\t" + Hex.toHexString(ss1));
+        System.out.println("Bobs shared secret:\t" + Hex.toHexString(ss2)+"\n");
+        
+        frodo.setParams(AlgorithmParameters.frodo1344());
+        frodo.createKeypair();
+        ss1 = frodo.keyEncap();
+        ss2 = frodo.keyDecap();
+        if (!Arrays.equals(ss1, ss2)) {
+            throw new RuntimeException("Shared secrets do not match");
+        }
+        
+        System.out.println("FrodoKEM 1344");
+        System.out.println("Auto-generated mu:\t" + Hex.toHexString(frodo.getMu()));
+        System.out.println("Alices shared secret:\t" + Hex.toHexString(ss1));
+        System.out.println("Bobs shared secret:\t" + Hex.toHexString(ss2)+"\n");
+        
         // Arrays.asList(cypher).retainAll(Arrays.asList(decoded));
         // assert(cypher.length == 0);
     }
 
-    public FrodoKEM() {
+    public FrodoKEM(AlgorithmParameters params) {
         rand = new SecureRandom();
         // create parameters according to Frodo640 spec
-        params = new AlgorithmParameters();
-
-        params.no = 640;
-        params.D = 15;
-        params.q = 0x7fff;
-        params.B = 2;
-        params.stripeStep = 8;
-        params.nbar = 8;
-        params.lenSeedA = 16;
-        params.cryptoBytes = 16;
-        params.lenX = 2;
-        params.bytesMU = (params.B * params.nbar * params.nbar) / 8;
-        params.CDF_TABLE = new int[] { 4643, 13363, 20579, 25843, 29227, 31145, 32103, 32525, 32689, 32745, 32762,
-                32766, 32767 };
-        params.secretKeyBytes = 19888; // sizeof(s) + CRYPTO_PUBLICKEYBYTES + 2*PARAMS_N*PARAMS_NBAR
-                                       // + BYTES_PKHASH
-        params.publicKeyBytes = 9616; // sizeof(seed_A) + (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8
-        params.cypherTextBytes = 9720;
-
-        cypherText = new byte[params.cypherTextBytes];
-
+        this.params = params;
     }
 
+    
+    /**
+     * Generate a public and secret key for the KEM
+     */
     public void createKeypair() {
         SHAKEDigest shake = new SHAKEDigest();
         byte[] pkBytes = new byte[params.publicKeyBytes];
@@ -99,17 +116,18 @@ public class FrodoKEM {
     }
 
     /**
-     * FrodoKem Key Encapsulation
+     * FrodoKem Key Encapsulation Generate a random seed and produces a cyphertext. Returns shared
+     * secret using the public key
      * 
-     * @param ct buffer array for cyphertext
      * @return the shared secret
      **/
     public byte[] keyEncap() {
 
         SHAKEDigest shake = new SHAKEDigest();
         byte[] sharedSecret = new byte[params.cryptoBytes];
-        
-        byte[] seed = new byte[((params.no * params.nbar)*2 + params.nbar * params.nbar)*params.lenX];
+        cypherText = new byte[params.cypherTextBytes];
+
+        byte[] seed = new byte[((params.no * params.nbar) * 2 + params.nbar * params.nbar) * params.lenX];
 
         short[][] Sp, Ep, Epp;
         short[][] B, Bp, V, C;
@@ -142,8 +160,7 @@ public class FrodoKEM {
         ct1 = pack(Bp);
 
         // Generate Epp, and compute V = Sp*B + Epp
-        Epp = sampleMatrix(Arrays.copyOfRange(seed, lenSeed * 2, seed.length), params.nbar,
-                params.nbar);
+        Epp = sampleMatrix(Arrays.copyOfRange(seed, lenSeed * 2, seed.length), params.nbar, params.nbar);
         B = unpack(pk.getMatrixB(), params.no, params.nbar);
         V = multiplyAddMatrices(Sp, B, Epp);
 
@@ -167,7 +184,12 @@ public class FrodoKEM {
         return sharedSecret;
     }
 
-    private byte[] keyDecap() {
+    /**
+     * FrodoKem Key Decapsulation Return the shared secret from cyphertext using the secret key
+     * 
+     * @return shared secret as byte array
+     */
+    public byte[] keyDecap() {
         SHAKEDigest shake = new SHAKEDigest();
         byte[] sharedSecret = new byte[params.cryptoBytes];
         byte[] G2In = new byte[params.cryptoBytes + params.bytesMU];
@@ -187,7 +209,7 @@ public class FrodoKEM {
 
         Bp = unpack(ct1, params.nbar, params.no);
         C = unpack(ct2, params.nbar, params.nbar);
-      
+
         W = multiply(Bp, sk.getMatrixS());
         W = sub(C, W);
         muPrime = decode(W);
@@ -206,12 +228,11 @@ public class FrodoKEM {
 
         int lenSeed = params.nbar * params.no * params.lenX;
         Sp = sampleMatrix(Arrays.copyOfRange(seed, 0, lenSeed), params.nbar, params.no);
-        Ep = sampleMatrix(Arrays.copyOfRange(seed, lenSeed, lenSeed*2), params.nbar, params.no);
+        Ep = sampleMatrix(Arrays.copyOfRange(seed, lenSeed, lenSeed * 2), params.nbar, params.no);
         BBp = multiplyAddMatrices(Sp, genMatrixA(pk.getSeedA()), Ep);
 
         // Generate Epp, and compute W = Sp*B + Epp
-        Epp = sampleMatrix(Arrays.copyOfRange(seed, lenSeed * 2, seed.length), params.nbar,
-                params.nbar);
+        Epp = sampleMatrix(Arrays.copyOfRange(seed, lenSeed * 2, seed.length), params.nbar, params.nbar);
         B = unpack(pk.getMatrixB(), params.no, params.nbar);
         W = multiplyAddMatrices(Sp, B, Epp);
 
@@ -222,7 +243,7 @@ public class FrodoKEM {
         System.arraycopy(cypherText, 0, fin, 0, cypherText.length);
 
         boolean bool1 = Arrays.deepEquals(Bp, BBp);
-        
+
         // Is (Bp == BBp & C == CC) = true
         if (bool1 == true && Arrays.deepEquals(C, CC) == true) {
             // Load k' to do ss = F(ct || k')
@@ -264,6 +285,11 @@ public class FrodoKEM {
         return R;
     }
 
+    /**
+     * Sample a value from the Gaussian noise distribution (CDF_TABLE)
+     * @return integer sample value
+     **/
+
     private int sample(int r) {
         int s = 0;
         int t = r >>> 1; // Drop the least significant bit
@@ -278,7 +304,9 @@ public class FrodoKEM {
         return s;
     }
 
-    /** Encode a byte string to a m-by-n matrix **/
+    /** Encode a byte array to a m-by-n matrix 
+     *  @returns the encoded matrix 
+      **/
     private short[][] encode(byte[] m) {
         int i, j, k;
         short[][] out = new short[params.nbar][params.nbar];
@@ -295,13 +323,16 @@ public class FrodoKEM {
                         temp |= 1 << k;
                     }
                 }
-                out[i][j] = (short) ( temp * (1 << (params.D-params.B)) & params.q);
+                out[i][j] = (short) (temp * (1 << (params.D - params.B)) & params.q);
             }
         }
 
         return out;
     }
 
+    /** Decode a n-by-m matrix to a bit string (stored as a byte array)
+     *  @returns the byte array
+     * **/
     private byte[] decode(short[][] m) {
         byte[] out = new byte[params.bytesMU];
 
@@ -310,10 +341,10 @@ public class FrodoKEM {
 
         for (i = 0; i < m.length; i++) {
             for (j = 0; j < m[0].length; j++) {
-                int b = 1 << params.B; 
-                int d = 1 << (params.D-params.B);
+                int b = 1 << params.B;
+                int d = 1 << (params.D - params.B);
                 float temp = Math.round((float) m[i][j] / (float) d) & (b - 1);
-                
+
                 for (k = 0; k < params.B; k++) {
                     if (((int) (temp) & (1 << k)) != 0) {
                         int index = ((i * params.nbar + j) * params.B + k) / 8;
@@ -325,7 +356,10 @@ public class FrodoKEM {
         }
         return out;
     }
-
+    
+    /** multiply two matrices 
+     *  @returns C = A * B  
+     **/
     public short[][] multiply(short[][] A, short[][] B) {
         short[][] out = new short[A.length][B[0].length];
 
@@ -451,207 +485,43 @@ public class FrodoKEM {
                 }
             }
         }
-=======
-
-public class FrodoKEM {
-
-  
-
-    private AlgorithmParameters params;
-    private SecureRandom rand;
-
-    byte[] pk, sk;
-
-    private short[] B;
-    private short[] S;
-    private short[] E;
-
-    private byte[] randomness;
-    private byte[] shake_input_seedSE;
-    private byte[] pk_seedA;
-    private short[] sk_S;
-
-    public static void main(String[] args) {
-        FrodoKEM frodo = new FrodoKEM();
-    }
-
-    public FrodoKEM() {
-        rand = new SecureRandom();
-        SHAKEDigest shake = new SHAKEDigest();
-        // create parameters according to Frodo640 spec
-        params = new AlgorithmParameters();
-
-        params.no = 640;
-        params.logQ = 15;
-        params.q = 1 << params.logQ;
-        params.extractedBits = 2;
-        params.stripeStep = 8;
-        params.nbar = 8;
-        params.lenSeedA = 16;
-        params.cryptoBytes = 16;
-        params.bytesMU = (params.extractedBits * params.nbar * params.nbar) / 8;
-        params.CDF_TABLE = new short[] { 4643, 13363, 20579, 25843, 29227, 31145, 32103, 32525, 32689, 32745, 32762,
-                32766, 32767 };
-        params.secretKeyBytes = 43088; // sizeof(s) + CRYPTO_PUBLICKEYBYTES +
-        // 2*PARAMS_N*PARAMS_NBAR + BYTES_PKHASH
-        params.publicKeyBytes = 21520; // sizeof(seed_A) +
-        // (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8
-        
-        pk = new byte[params.publicKeyBytes];
-        sk = new byte[params.secretKeyBytes];
-        //pk_seedA = new byte[CRYPTO_PUBLICKEYBYTES];
-        S = new short[2 * params.no * params.nbar];
-        B = new short[params.no * params.nbar];
-        E = new short[params.no * params.nbar];
-        randomness = new byte[2 * params.cryptoBytes + params.lenSeedA];
-        shake_input_seedSE = new byte[1 + params.cryptoBytes];
-        rand.nextBytes(randomness);
-
-        shake.update(randomness, 2 * params.cryptoBytes, params.lenSeedA);
-        shake.doFinal(pk, 0, params.lenSeedA);
-
-        shake_input_seedSE[0] = 0x5F;
-        System.arraycopy(randomness, params.cryptoBytes, shake_input_seedSE, 1, params.cryptoBytes);
-        shakeToShort(shake_input_seedSE, S);
-
-        S = sampleMatrix(S, S.length);
-        System.arraycopy(S, params.no * params.nbar, E, 0, params.no * params.nbar);
-
-        multiplyASplusE(B, S, E, pk);
-
-        byte[] pk_b = pack(B, B.length, (byte) params.logQ);
-
-        // Encode second part of public key
-        System.arraycopy(pk_b, 0, pk, params.lenSeedA, pk_b.length);
-
-        //add s, pk and S to secret key
-        System.arraycopy(randomness, 0, sk, 0, params.cryptoBytes);
-        System.arraycopy(pk, 0, sk, params.cryptoBytes, params.publicKeyBytes);
-        
-        int j = params.cryptoBytes + params.publicKeyBytes;
-        for (int i = 0; i < S.length; i ++) {
-            sk[j++] =(byte) (S[i] >> 8);
-            sk[j++] = (byte) (S[i] & 0xFF);
-        }
-              
-        shake.update(pk, 0, pk.length);
-        shake.doFinal(sk, params.cryptoBytes + params.publicKeyBytes + S.length);
-
-        // *randomness_s = &randomness[0]; // contains secret data
-        // *randomness_seedSE = &randomness[CRYPTO_BYTES]; // contains secret data
-        // *randomness_z = &randomness[2*CRYPTO_BYTES];
-    }
-
-    private void shakeToShort(byte[] input, short[] output) {
-        shakeToShort(input, 0, input.length, output, 0, output.length);
-    }
-
-    private void shakeToShort(byte[] input, int inOffset, int inlen, short[] output, int outOffset, int outlen) {
-        SHAKEDigest d = new SHAKEDigest();
-        byte[] temp = new byte[2*outlen];
-        d.update(input, inOffset, inlen);
-        d.doFinal(temp, 0, temp.length);
-
-        int j = outOffset;
-        for (int i = 0; i < temp.length; i += 2) {
-            output[j++] = (short) ((temp[i] << 8) | temp[i + 1]);
-        }
-    }
-
-    /**
-     * Fills a vector with n samples from the noise distribution which requires 16 bits to sample.
-     * The distribution is specified by its CDF. Input: pseudo-random values (2*n bytes) passed in
-     * s.
-     * 
-     * @param s vector with pseudo-radomly generated values
-     * @param length size of output vector
-     * @return vector with sampled values
-     **/
-
-    private short[] sampleMatrix(short[] s, int length) {
-
-        int i, j;
-        short[] r = new short[s.length];
-
-        for (i = 0; i < length; ++i) {
-            short sample = 0;
-            short prnd = (short) (s[i] >> 1); // Drop the least significant bit
-            short sign = (short) (s[i] & 0x1); // Pick the least significant bit
-
-            // No need to compare with the last value.
-            for (j = 0; j < (params.CDF_TABLE.length - 1); j++) {
-                // Constant time comparison: 1 if CDF_TABLE[j] < s, 0 otherwise. Uses the fact that
-                // CDF_TABLE[j] and s fit in 15 bits.
-                sample += (short) (params.CDF_TABLE[j] - prnd) >> 15;
-            }
-            // Assuming that sign is either 0 or 1, flips sample if sign = 1
-            r[i] = (short) (((-sign) ^ sample) + sign);
-        }
-        return r;
-    }
-
-    private void multiplyASplusE(short[] out, short[] s, short[] e, byte[] seedA) {
-        SHAKEDigest shake = new SHAKEDigest();
-        short[] A = new short[params.no * params.no];
-
-        // generate matrix A
-        byte seedASeparated[] = new byte[2 + params.lenSeedA];
-        System.arraycopy(seedA, 0, seedASeparated, 2, params.lenSeedA);
-        for (int i = 0; i < params.no; i++) {
-            seedASeparated[0] = (byte) (i & 0xFF);
-            seedASeparated[1] = (byte) (i >> 8);
-            shakeToShort(seedASeparated, 0, seedASeparated.length, A, i * params.no, params.no);
-        }
-
-        // Matrix multiplication-addition A*s + e
-        for (int i = 0; i < params.no; i++) {
-            for (int k = 0; k < params.nbar; k++) {
-                short sum = 0;
-                for (int j = 0; j < params.no; j++) {
-                    sum += A[i * params.no + j] * s[k * params.no + j];
-                }
-                out[i * params.nbar + k] = (short) (sum + e[i * params.nbar + k]);
-            }
-        }
-    }
-
-    private byte[] pack(short[] input, int inlen, byte lsb) {
-        int i = 0; // whole bytes already filled in
-        int j = 0; // whole uint16_t already copied
-        short w = 0; // the leftover, not yet copied
-        byte bits = 0; // the number of lsb in w
-
-        byte[] out = new byte[params.publicKeyBytes - params.lenSeedA];
-
-        while (i < out.length && (j < inlen || ((j == inlen) && (bits > 0)))) {
-
-            byte b = 0; // bits in out[i] already filled in
-            while (b < 8) {
-                int nbits = Math.min(8 - b, bits);
-                short mask = (short) ((1 << nbits) - 1);
-                byte t = (byte) ((w >>> (bits - nbits)) & mask); // the bits to copy from w to out
-                out[i] = (byte) (out[i] + (t << (8 - b - nbits)));
-                b += nbits;
-                bits -= nbits;
-                w &= ~(mask << bits); // not strictly necessary; mostly for debugging
-
-                if (bits == 0) {
-                    if (j < inlen) {
-                        w = input[j];
-                        bits = lsb;
-                        j++;
-                    } else {
-                        break; // the input vector is exhausted
-                    }
-                }
-            }
-            if (b == 8) { // out[i] is filled in
-                i++;
-            }
-        }
-
->>>>>>> refs/remotes/origin/lwe
         return out;
     }
+    
+       
+    
+    
+    public AlgorithmParameters getParams() {
+        return params;
+    }
 
+    public void setParams(AlgorithmParameters params) {
+        this.params = params;
+    }
+
+    public PublicKey getPk() {
+        return pk;
+    }
+
+    public void setPk(PublicKey pk) {
+        this.pk = pk;
+    }
+
+    public SecretKey getSk() {
+        return sk;
+    }
+
+    public void setSk(SecretKey sk) {
+        this.sk = sk;
+    }
+
+    public byte[] getMu() {
+        return mu;
+    }
+
+    public void setMu(byte[] mu) {
+        this.mu = mu;
+    }
+    
+    
 }
